4ec900dc57cfa337538bdc0cd7e1e571b6380576
Christian Windolf <christianwindolf@web.de>
2017-11-24 17:35:57 +0100

    add basic app structure

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,6 +1,22 @@
 <template>
-  <div id="app">
-  </div>
+  <v-app id="bfb" dark>
+    <v-toolbar app fixed clipped-left>
+      <v-toolbar-title>
+        Battle For Berlin
+      </v-toolbar-title>
+      <v-spacer />
+      <v-toolbar-items class="hidden-sm-and-down">
+        <v-btn href="https://github.com/NemoNessuno/BattleForBerlin" flat>
+          <img src="/static/images/github_logo.png" class="github-link-img" />
+        </v-btn>
+      </v-toolbar-items>
+    </v-toolbar>
+    <v-content>
+    </v-content>
+    <v-footer>
+      <span>&copy; 2017, S&ouml;ren Titze, Christian Windolf</span>
+    </v-footer>
+  </v-app>
 </template>
 
 <script>
@@ -11,4 +27,10 @@ export default {
 
 <style>
 @import 'vuetify/dist/vuetify.min.css'
+</style>
+
+<style>
+.github-link-img {
+  height: 3em;
+}
 </style>
