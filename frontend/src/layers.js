2955660f7ba1685abc437d38d909ae2ac034587c
Christian Windolf <christianwindolf@web.de>
2017-12-02 17:22:49 +0100

    colorize map by party

--- frontend/src/layers.js
+++ frontend/src/layers.js
@@ -1,19 +1,25 @@
 import L from 'leaflet'
 
+const PARTY_COLORS = {
+  afd: '#2B9FDB',
+  gruene: '#4E8935',
+  spd: '#DB0E2A',
+  cdu: '#000000',
+  fdp: '#FEE943',
+  die_linke: '#D8101F'
+}
+
 export class DistrictLayer {
   constructor (districts, config) {
     this.config = config || {}
     this.config.defaultStyle = this.config.defaultStyle || {
-      style () { return {color: '#FF7800', weight: 1, opacity: 0.65} }
+      style: styleDistrict
     }
     this.config.emphazisedStyle = this.config.emphazisedStyle || {
       style () { return {color: '#0078FF', weight: 1, opacity: 0.7} }
     }
-    this.layers = L.layerGroup(Array(districts).map(function (district, idx) {
+    this.layers = L.layerGroup(Array(districts).map(function (district) {
       const $this = this
-      if (idx === 1) {
-        console.log(district)
-      }
       const layer = L.geoJSON(district, this.config.defaultStyle)
       layer.on('click', function ({layer}) {
         $this.selectDistrict.bind($this)(layer)
@@ -29,13 +35,26 @@ export class DistrictLayer {
     }
     this.reset()
     this._selectedLayer = layer
-    layer.setStyle(this.config.emphazisedStyle.style())
+    layer.setStyle(this.config.emphazisedStyle.style(layer.feature))
   }
 
   reset () {
     if (this._selectedLayer) {
-      this._selectedLayer.setStyle(this.config.defaultStyle.style())
+      this._selectedLayer.setStyle(this.config.defaultStyle.style(this._selectedLayer.feature))
     }
     this._selectedLayer = undefined
   }
+}
+
+function styleDistrict ({geometry}) {
+  const {properties} = geometry
+  let partyMax = ''
+  let resultMax = 0
+  Object.keys(properties.result).forEach(function (party) {
+    if (properties.result[party] > resultMax) {
+      partyMax = party
+      resultMax = properties.result[party]
+    }
+  })
+  return {color: PARTY_COLORS[partyMax], weight: 1, opacity: 0.65}
 }
