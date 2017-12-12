9aecd92bac4369db858fd33981327251faa50f35
Christian Windolf <christianwindolf@web.de>
2017-12-12 11:20:30 +0100

    also fetch counties

--- frontend/src/backend.js
+++ frontend/src/backend.js
@@ -1,8 +1,9 @@
 export function fetchDistricts () {
   const ballotBox = fetch('/api/districts/ballot').then(resp => resp.json())
   const letters = fetch('/api/districts/letters').then(resp => resp.json())
-  return Promise.all([ballotBox, letters]).then(function (districts) {
-    const [ballot, letters] = districts
-    return {ballot, letters}
+  const counties = fetch('/api/counties').then(resp => resp.json())
+  return Promise.all([ballotBox, letters, counties]).then(function (districts) {
+    const [ballot, letters, counties] = districts
+    return {ballot, letters, counties}
   })
 }
