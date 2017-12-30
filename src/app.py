0601130a299a69bd11eebfc7fdf3052a3c773a61
Christian Windolf <christianwindolf@web.de>
2017-12-30 13:27:49 +0100

    remove unnused import

--- src/app.py
+++ src/app.py
@@ -1,18 +1,14 @@
 import json
 from flask import Flask, render_template, jsonify
-from geoalchemy2 import functions
 
-from database.db_handler import db_session
 from database.models import LetterDistrict, UrnDistrict
 from database.db_helper import get_district_geojson, get_county_geojson
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def start():
     return render_template('index.html')
-
 
 @app.route('/api/districts/ballot')
 def urn_districts():
@@ -23,11 +19,9 @@ def urn_districts():
 def letter_districts():
     return jsonify(get_district_geojson(LetterDistrict))
 
-
 @app.route('/api/counties')
 def counties():
     return jsonify(get_county_geojson())
-
 
 if __name__ == '__main__':
     app.debug = True
