35b2f1db750133685ab7398ea99ab43cfbac6d8e
Christian Windolf <christianwindolf@web.de>
2017-12-30 14:00:22 +0100

    Let the greens win Kreuzb./Fhain

--- src/database/db_helper.py
+++ src/database/db_helper.py
@@ -26,19 +26,20 @@ def get_county_geojson():
     l_query = db_session.query(
         LetterDistrict.bwk,
         functions.ST_AsGeoJSON(functions.ST_Union(LetterDistrict.geom)).label("geom"),
-        *[func.sum(getattr(LetterDistrict, label_name)).label(label_name) for label_name in labels]
+        *sum_party_results(LetterDistrict)
     ).group_by(LetterDistrict.bwk)
 
     u_query = db_session.query(
         UrnDistrict.bwk,
-        *[func.sum(getattr(UrnDistrict, label_name)).label(label_name) for label_name in labels]
+        *sum_party_results(UrnDistrict)
     ).group_by(UrnDistrict.bwk)
 
     u_results = u_query.all()
+    u_dict = {r[0]:r for r in u_results}
     geojsons = []
     for row in l_query.all():
         geojson = json.loads(row.geom)
-        u_result = next(x for x in u_results if lambda result: result.bwk == row.bwk)
+        u_result = u_dict[row.bwk]
         geojson["properties"] = {
             "bwk": row.bwk,
             "u_result": dict((label, getattr(u_result, label)) for label in labels),
@@ -47,3 +48,9 @@ def get_county_geojson():
         geojsons.append(geojson)
 
     return geojsons
+
+def sum_party_results(model):
+    parties = [
+        "cdu", "spd", "gruene", "die_linke", "fdp", "afd"
+    ]
+    return [func.sum(getattr(model, party)).label(party) for party in parties]
