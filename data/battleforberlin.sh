f2551b687a7ef1887de2fae3d4b33c0095a10953
Christian Windolf <christianwindolf@web.de>
2018-01-11 17:56:40 +0100

    stop importing extensions

--- data/battleforberlin.sh
+++ data/battleforberlin.sh
@@ -2,6 +2,4 @@
 
 set -e
 
-echo 'create database battle_for_berlin;' | psql -U postgres
-psql -U postgres -d battle_for_berlin < /data/database.sql
-psql -U postgres -d battle_for_berlin < /data/extensions.sql
+psql -U postgres < /data/database.sql
