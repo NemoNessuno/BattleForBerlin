ef62ec82136d0d8151fa589e8dd73e297eb7f654
Christian Windolf <christianwindolf@web.de>
2018-01-17 18:43:13 +0100

    give AGDBS logo a nice animation

--- frontend/src/components/Welcome.vue
+++ frontend/src/components/Welcome.vue
@@ -2,13 +2,13 @@
   <div>
     <svg height="500" width="500" >
       <svg x="200" >
-        <path class="fu-green" d="M290 10 H 22 Q 10 10 10 22 V290 H278 Q 290 290 290 278  Z"  />
+        <path class="fu-green show first" d="M290 10 H 22 Q 10 10 10 22 V290 H278 Q 290 290 290 278  Z"  />
       </svg>
       <svg x="100" y="100">
-        <path class="fu-lightblue" d="M290 10 H 22 Q 10 10 10 22 V290 H278 Q 290 290 290 278  Z"  />
+        <path class="fu-lightblue show second" d="M290 10 H 22 Q 10 10 10 22 V290 H278 Q 290 290 290 278  Z"  />
       </svg>
       <svg y="200">
-        <path class="fu-darkblue" d="M290 10 H 22 Q 10 10 10 22 V290 H278 Q 290 290 290 278  Z"  />
+        <path class="fu-darkblue show third" d="M290 10 H 22 Q 10 10 10 22 V290 H278 Q 290 290 290 278  Z"  />
       </svg>
     </svg>
   </div>
@@ -21,20 +21,38 @@
 </script>
 
 <style lang="sass" scoped>
+@keyframes fadein
+  from
+    opacity: 0
+  to
+    opacity: 1
 div
   background-color: white
   width: 100%
+  padding: 1em
 svg
   display: block
   margin: auto
   stroke-width: 20px
 path
-  fill: none;
+  fill: none
   &.fu-green
     stroke: #99CC00
   &.fu-lightblue
     stroke: #0066CC
   &.fu-darkblue
     stroke: #003366
+.show
+  animation-name: fadein
+  animation-duration: 0.5s
+  animation-timing-function: ease
+  animation-fill-mode: backwards
+  &.first
+    animation-delay: 0
+  &.second
+    animation-delay: 0.5s
+  &.third
+    animation-delay: 1s
+
 
 </style>
