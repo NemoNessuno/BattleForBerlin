d1b02358ad1ac1a971dfb5681b9990c4cdc4e60c
Christian Windolf <christianwindolf@web.de>
2018-01-26 21:36:35 +0100

    add animation map to routes

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -4,11 +4,13 @@ import LeafletMap from './components/LeafletMap'
 import GerryMander from './components/GerryMander'
 import Onboarding from './components/Onboarding'
 import Welcome from './components/Welcome'
+import AnimationMap from './components/AnimationMap'
 
 const routes = [
   {path: '/map/:identifier?', component: LeafletMap},
   {path: '/votingdistrict/:identifier', component: GerryMander},
   {path: '/info', component: Onboarding},
+  {path: '/animation', component: AnimationMap},
   {path: '/', component: Welcome}
 ]
 
