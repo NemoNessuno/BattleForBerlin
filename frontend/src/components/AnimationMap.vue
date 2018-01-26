9360e6ad812688ba808a66838022ee0cd67b6832
Christian Windolf <christianwindolf@web.de>
2018-01-26 21:33:49 +0100

    create base animation map

---
+++ frontend/src/components/AnimationMap.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="map2" />
+</template>
+<script>
+  import {buildLeafletMap} from '../helpers'
+  export default {
+    name: 'animation-map',
+    data () {
+      return {
+      }
+    },
+    mounted () {
+      this.map = buildLeafletMap(this.$el)
+    }
+  }
+</script>
+
+<style lang="sass" scoped>
+#map2
+  height: 600px
+  width: 100%
+
+</style>
