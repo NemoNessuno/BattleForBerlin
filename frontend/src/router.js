f6c5ff04f342a1f62d228981ddb92e89d4cef3bf
Christian Windolf <christianwindolf@web.de>
2018-02-04 13:48:18 +0100

    annotate routes with meta data

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -7,11 +7,50 @@ import Welcome from './components/Welcome'
 import AnimationMap from './components/AnimationMap'
 
 const routes = [
-  {path: '/map/:identifier?', component: LeafletMap},
-  {path: '/votingdistrict/:identifier', component: GerryMander},
-  {path: '/info', component: Onboarding},
-  {path: '/animation', component: AnimationMap},
-  {path: '/', component: Welcome}
+  {
+    path: '/map/:identifier?',
+    component: LeafletMap,
+    name: 'map',
+    meta: {
+      title: 'Battle For Berlin',
+      footerInvisible: true
+    }
+  },
+  {
+    path: '/votingdistrict/:identifier',
+    component: GerryMander,
+    name: 'gerrymander',
+    meta: {
+      icon: 'arrow_back',
+      title: 'bezirk'
+    }
+  },
+  {
+    path: '/info',
+    component: Onboarding,
+    meta: {
+      title: 'Gerrymandering Information',
+      icon: 'arrow_back'
+    }
+  },
+  {
+    path: '/animation',
+    component: AnimationMap,
+    name: 'animation',
+    meta: {
+      title: 'GerryMandering',
+      icon: 'arrow_back',
+      footerInvisible: true
+    }
+  },
+  {
+    path: '/',
+    component: Welcome,
+    name: 'welcome',
+    meta: {
+      title: 'Battle For Berlin'
+    }
+  }
 ]
 
 export default new VueRouter({routes})
